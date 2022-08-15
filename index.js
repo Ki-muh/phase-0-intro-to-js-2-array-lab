3f6ffff3a9176fcad99629811b751b06871224f1
Martin Kamau <martinkimu09@gmail.com>
2022-08-16 02:35:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+function appendCat(name) {
+    const appendNewCat = [...cats, name];
+    return appendNewCat;
+}
+
+function prependCat(name) {
+    const prependNewCat = [name, ...cats];
+    return prependNewCat;
+}
 
 function destructivelyAppendCat(Ralph) {
-    cats.push(Ralph)
-    return Ralph
+    cats.push(Ralph);
+    return Ralph;
 }
 function destructivelyPrependCat(Bob) {
-    cats.unshift(Bob)
-    return Bob
+    cats.unshift(Bob);
+    return Bob;
 }
 function destructivelyRemoveLastCat(name) {
-    cats.pop(name)
-    return name
+    cats.pop(name);
+    return name;
 }
 function destructivelyRemoveFirstCat(name) {
-    cats.shift(name)
-    return name
+    cats.shift(name);
+    return name;
 }
-function removeLastCat(Garfield) {
-    cats.splice(-Garfield)
-    return [cats]
+function removeLastCat() {
+    return cats.slice(0,cats.length-1);
+    
 }
-function removeFirstCat(Milo) {
-    cats.slice(-Milo)
-    return [cats]
+function removeFirstCat() {
+    return cats.slice(1);
+    
 }
